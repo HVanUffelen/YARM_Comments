5900f7f0aa47ba4c1f9e824ceec4179cd2289ecb
vanuffh7 <herbert.van-uffelen@univie.ac.at>
2022-09-22 10:16:42 +0200

    add details_comments.blade

---
+++ resources/views/inc/details_comments.blade.php
@@ -0,0 +1,12 @@
+<?php
+@if (auth()->user() &&
+            (auth()->user()->roles()->first()->role != 'Website' && config('yarm.core') != True))
+    <div class="card mt-3">
+        <h4 class="card-header">@lang('Comments')</h4>
+        <div class="card-body">
+
+            @comments(['model' => $ref])
+
+        </div>
+    </div>
+@endif
