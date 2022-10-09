74982dd943ae29655f1c1fbb6818cee492ee1aa7
vanuffh7 <herbert.van-uffelen@univie.ac.at>
2022-10-09 17:26:41 +0200

    Error

--- resources/views/inc/details_comments.blade.php
+++ resources/views/inc/details_comments.blade.php
@@ -1,5 +1,5 @@
 @if (auth()->user() &&
-            (auth()->user()->roles()->first()->role != 'Website' && config('yarm.core') != True))
+            (auth()->user()->roles()->first()->role != 'Website' && config('yarm.comments') == True))
     <div class="card mt-3">
         <h4 class="card-header">@lang('Comments')</h4>
         <div class="card-body">
